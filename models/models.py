170872ab1b467d8f59ecfb7966c856e8b4823d1e
Ilya <105770380+Artivus2@users.noreply.github.com>
2024-11-18 12:10:04 +0700

    Update models.py

--- models/models.py
+++ models/models.py
@@ -1,17 +1,23 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, HttpUrl
 from hashlib import sha256
 import mysql
 from mysql.connector import errorcode
 import logging
 import time
 
+
+class Logout(BaseModel):
+    token: str
+
+
 class Register(BaseModel):
     login: str
     password: str
     telegram: str
 
+
 class Login(BaseModel):
-    email: str
+    email: EmailStr
     password: str
 
 
@@ -25,23 +31,29 @@ class JwtRequest(BaseModel):
     email: str
     password: str
 
-    def get_db_connection(self):
-        config = {
-            'user': 'greenavi_user',
-            'password': 'tb7x3Er5PQ',
-            'host': '127.0.0.1',
-            'database': 'greenavi_app',
-            'raise_on_warnings': True
-        }
-        cnx = mysql.connector.connect(**config)
-        try:
-            cnx = mysql.connector.connect(config)
-        except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your user name or password")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
-            else:
-                print(err)
+
+def get_db_connection():
+    config = {
+        'user': 'greenavi_user',
+        'password': 'tb7x3Er5PQ',
+        'host': '127.0.0.1',
+        'database': 'greenavi_app',
+        'raise_on_warnings': True
+    }
+    cnx = mysql.connector.connect(**config)
+    try:
+        cnx = mysql.connector.connect(config)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
         else:
-            cnx.close()
+            print(err)
+    else:
+        cnx.close()
+    return cnx
+
+
+class GetDbConnection():
+    pass
