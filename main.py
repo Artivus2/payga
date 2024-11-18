b4497049cf5b9ef48e952f21e02e7c1473867366
Ilya <105770380+Artivus2@users.noreply.github.com>
2024-11-18 09:04:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,63 +5,69 @@ import requests
 import json
 from fastapi import FastAPI, HTTPException, Depends, Header, Query
 
-
+base_url = 'https://greenavi.com'
 app = FastAPI()
+
 
 @app.get("/")
 async def route():
     return "root"
 
+
 @app.get("/api/v1/test")
-async def test():
+async def api1():
     return "all ok"
 
 
 @app.get("/api/v1/login")
-async def login():
-    base_url = 'https://greenavi.com'
+async def login(request: models.models.Login):
+    '''
+    email
+    password
+    :return:
+    {код} 
+    '''
     api_url = f'{base_url}/api/user/login'
     headers = {
         'Content-Type': 'application/json'
     }
-    email = 'artivus3@yandex.ru'
-    password = 'Adm142!@'
     payload = {
-        'email': 'artivus3@yandex.ru', #req
-        'password': 'Adm142!@' #req
+        'email': request.email,  # req
+        'password': request.password  # req
     }
-    print(api_url)
+    response = requests.post(api_url, headers=headers, data=json.dumps(payload))
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail=response.json())
+    return response.json()
+
+
+@app.post("/jwt")
+async def get_jwt_token(request: models.models.JwtRequest):
+    '''
+    запрос токена авторизации
+    :param request: email, password
+    :return: 
+    token
+    base_url = 'https://greenavi.com'
+    api_url = f'{base_url}/api/user/send-code'
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    payload = {
+        'email': request.email, #req
+        'password': request.password #req
+    }
     response = requests.post(api_url, headers=headers, data=json.dumps(payload))
     if response.status_code != 200:
         raise HTTPException(status_code=response.status_code, detail=response.json())
     print(response.json())
     return response.json()
 
-
-@app.get("/jwt")
-def get_jwt_token(request: models.models.JwtRequest):
-    email = 'artivus3@yandex.ru'
-    password = 'Adm142!@'
-    base_url = 'https://greenavi.com'
-    api_url = f'{base_url}/api/user/login'
-    # headers = {
-    #     'Content-Type': 'application/json'
-    # }
-    # payload = {
-    #     'email': email, #req
-    #     'password': password #req
-    # }
-    # response = requests.post(api_url, headers=headers, data=json.dumps(payload))
-    # if response.status_code != 200:
-    #     raise HTTPException(status_code=response.status_code, detail=response.json())
-    # print(response.json())
-    #return response.json()
-    return api_url
-
 async def main():
     config = uvicorn.Config("main:app", port=5001, log_level="info", reload=True)
     server = uvicorn.Server(config)
     await server.serve()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
