aafb4873d7461dbf0d260872104440f653073bca
Ilya <105770380+Artivus2@users.noreply.github.com>
2024-11-18 23:07:56 +0700

    ok

--- main.py
+++ main.py
@@ -1,36 +1,99 @@
 import asyncio
+import mysql.connector.aio as cpy_async
 import uvicorn
 import models.models
 import requests
 import json
 from fastapi import FastAPI, HTTPException, Depends, Header, Query
+import mysql
 
 
+base_url = 'http://localhost'
 app = FastAPI()
+
 
 @app.get("/")
 async def route():
     return "root"
 
+
 @app.get("/api/v1/test")
-async def test():
-    return "all ok"
+async def api1():
+    config = {
+        'user': 'greenavi_user',
+        'password': 'tb7x3Er5PQ',
+        'host': '127.0.0.1',
+        'port': 3306,
+        'database': 'greenavi_app',
+        'raise_on_warnings': True
+    }
+    #cnx = mysql.connector.connect(**config)
+    async with await cpy_async.connect(**config) as cnx:
+        async with await cnx.cursor() as cur:
+            await cur.execute("SELECT @@version")
+            print(await cur.fetchall())
 
-
-@app.get("/api/v1/login")
-async def login():
-    base_url = 'https://greenavi.com'
-    api_url = f'{base_url}/api/user/login'
+@app.post("/api/v1/user/logout")
+async def logout(request: models.models.Logout):
+    """
+    Логаут
+    :param request:
+    token
+    :return:
+    """
+    api_url = f'{base_url}/api/user/logout'
     headers = {
         'Content-Type': 'application/json'
     }
-    email = 'artivus3@yandex.ru'
-    password = 'Adm142!@'
     payload = {
-        'email': 'artivus3@yandex.ru', #req
-        'password': 'Adm142!@' #req
+        'token': request.token,  # req
     }
-    print(api_url)
+    response = requests.post(api_url, headers=headers, data=json.dumps(payload))
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail=response.json())
+    return response.json()
+
+
+@app.post("/api/v1/user/register-request")
+async def register_request(request: models.models.RegisterRequest):
+    """
+    login: Логин,
+    email: емаил,
+    telegram: телеграм
+    affiliate: реф,
+    password: пароль,
+    :param request:
+    :return:
+    {comment возращает ссылку на регистрацию после подтверждения администратора, отправляет на почту пользователю}
+    """
+    hashed_password = await hashFromYii2(request.password)
+    #hashed_password = '1212'
+    payload = {
+        'email': request.email,  # req
+        'login': request.login,  # req
+        'telegram': request.telegram,  # req
+        'affiliate': request.affiliate,  # req
+        'password': hashed_password['password'],  # req
+    }
+    print(payload)
+    models.models.insert_new_user(**payload)
+
+
+
+
+async def hashFromYii2(password):
+    """
+    получить хешкод пароля из yii2
+    :param password:
+    :return:
+    """
+    api_url = f'{base_url}/api/user/get-password'
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    payload = {
+        'password': password  # req
+    }
     response = requests.post(api_url, headers=headers, data=json.dumps(payload))
     if response.status_code != 200:
         raise HTTPException(status_code=response.status_code, detail=response.json())
@@ -38,30 +101,58 @@ async def login():
     return response.json()
 
 
-@app.get("/jwt")
-def get_jwt_token(request: models.models.JwtRequest):
-    email = 'artivus3@yandex.ru'
-    password = 'Adm142!@'
-    base_url = 'https://greenavi.com'
+@app.post("/api/v1/user/login")
+async def login(request: models.models.Login):
+    """
+    email
+    password
+    :return:
+    {token}
+    """
     api_url = f'{base_url}/api/user/login'
-    # headers = {
-    #     'Content-Type': 'application/json'
-    # }
-    # payload = {
-    #     'email': email, #req
-    #     'password': password #req
-    # }
-    # response = requests.post(api_url, headers=headers, data=json.dumps(payload))
-    # if response.status_code != 200:
-    #     raise HTTPException(status_code=response.status_code, detail=response.json())
-    # print(response.json())
-    #return response.json()
-    return api_url
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    payload = {
+        'email': request.email,  # req
+        'password': request.password  # req
+    }
+    response = requests.post(api_url, headers=headers, data=json.dumps(payload))
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail=response.json())
+    return response.json()
+
+
+@app.post("/api/v1/user/code")
+async def code(request: models.models.Code):
+    """
+    запрос токена авторизации
+    :param request: email, password, code
+    :return:
+    {access_token}
+    """
+    api_url = f'{base_url}/api/user/code'
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    payload = {
+        'email': request.email,  # req
+        'password': request.password,  # req
+        'code': request.code  # req
+    }
+    print(payload)
+    response = requests.post(api_url, headers=headers, data=json.dumps(payload))
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail=response.json())
+    print(response.json())
+    return response.json()
+
 
 async def main():
     config = uvicorn.Config("main:app", port=5001, log_level="info", reload=True)
     server = uvicorn.Server(config)
     await server.serve()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
--- models/models.py
+++ models/models.py
@@ -1,9 +1,23 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl
 from hashlib import sha256
 import mysql
 from mysql.connector import errorcode
 import logging
 import time
+import mysql.connector as cpy
+
+
+
+class Logout(BaseModel):
+    token: str
+
+
+class RegisterRequest(BaseModel):
+    login: str
+    email: str
+    telegram: str
+    affiliate: str
+    password: str
 
 
 class Login(BaseModel):
@@ -11,7 +25,9 @@ class Login(BaseModel):
     password: str
 
 
-class SendCode(BaseModel):
+class Code(BaseModel):
+    email: str
+    password: str
     code: str
 
 
@@ -19,24 +35,38 @@ class JwtRequest(BaseModel):
     email: str
     password: str
 
-    def get_db_connection(self):
-        config = {
-            'user': 'greenavi_user',
-            'password': 'tb7x3Er5PQ',
-            'host': '127.0.0.1',
-            'database': 'greenavi_app',
-            'raise_on_warnings': True
-        }
 
-        cnx = mysql.connector.connect(**config)
-        try:
-            cnx = mysql.connector.connect(config)
-        except mysql.connector.Error as err:
-            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your user name or password")
-            elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
+def insert_new_user(**payload):
+    config = {
+        'user': 'greenavi_user',
+        'password': 'tb7x3Er5PQ',
+        'host': '127.0.0.1',
+        'database': 'greenavi_app',
+        'raise_on_warnings': True
+    }
+    with cpy.connect(**config) as cnx:
+        with cnx.cursor() as cur:
+            app_id = 3
+            comment = 'https://test.greenavi.com/confirm-register?login=' + str(payload['login'])
+            data_login = "SELECT * from user where login = '" + str(payload['login']) + "' or email = '" + payload['email'] + "'"
+            print(data_login)
+            cur.execute(data_login)
+            data = cur.fetchall()
+            print(data)
+            if not data:
+                data_string = "INSERT INTO user (login, email, password, comment, telegram, app_id) " \
+                              "VALUES ('" + str(payload['login']) + "','" + str(payload['email']) + \
+                              "','" + str(payload['password']) + "','" + str(comment) + "','" + str(payload['telegram']) \
+                              + "','" + str(app_id) + "')"
+                cur.execute(data_string)
+                cnx.commit()
+                cnx.close()
+                print(comment)
+                return comment
             else:
-                print(err)
-        else:
-            cnx.close()
+                cnx.close()
+                return "Пользователь уже существует"
+
+
+class GetDbConnection():
+    pass
